0d85d8cc0aa8d3279e3f822b1f3af5c7f2be2e95
gxaurab <oe.gaurab@gmail.com>
2025-06-30 08:29:20 +0545

    Contact Form Made Complet with Discord

---
+++ frontend/src/pages/ContactForm.tsx
@@ -0,0 +1,21 @@
+import ContactForm from "../components/ContactForm"
+import { motion } from "framer-motion"
+
+
+const Contact = () => {
+  return (
+    <div className="bg-lime-50 p-2">
+        <motion.h1 
+        initial= {{opacity:0, y: 100}}
+        whileInView={{opacity:1, y:0}}
+        transition={{duration: 1.5}}
+        viewport={{once:true}}
+        className='
+          text-4xl bg-lime-50 p-2 mb-3 sm:text-4xl text-center font-medium'>Contact Our Team
+          </motion.h1>
+        <ContactForm/>
+    </div>
+  )
+}
+
+export default Contact
